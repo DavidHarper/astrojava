2fa71e511f579229ef7e3a93879196cf77babafe
David Harper <david@obliquity.net>
2003-08-25 14:32:29 +0000

    Added Javadoc documentation to the JPLEphemeris class.
    git-svn-id: file:///disk2/repository/svn/trunk/astrojava@88 32807195-9110-437f-9ebb-0d7c4c47859f

--- com/obliquity/astronomy/JPLEphemeris.java
+++ com/obliquity/astronomy/JPLEphemeris.java
@@ -4,6 +4,28 @@ import java.io.*;
 import java.lang.*;
 import java.text.*;
 
+/**
+ * This class encapsulates a JPL planetary ephemeris such as DE200
+ * or DE405. It provides methods for evaluating the position and
+ * velocity of any object in the ephemeris at any time instant which
+ * is withing the range of the ephemeris.
+ * <P>
+ * An object of this class may be created directly from the binary
+ * JPL ephemeris files which are distributed by Myles Standish at
+ * the JPL ftp site: ftp://ssd.jpl.nasa.gov/pub/eph/export/unix
+ * <P>
+ * The user may specify a time span for which the ephemeris object
+ * should be able to return positions and velocities. Only the data
+ * records need to cover this time span will be loaded from the file.
+ * <P>
+ * This class is serializable, which means that once an instance has
+ * been created from a binary ephemeris file, it may be saved to disk
+ * and re-created via the ObjectInputStream.readObject method. This
+ * allows a subset of a JPL ephemeris to be serialized as part of a
+ * JAR package or retrieved by an applet or other network-aware
+ * Java application.
+ */
+
 public class JPLEphemeris implements Serializable {
     private double []limits = null;
     private int [][]offsets = null;
@@ -32,6 +54,20 @@ public class JPLEphemeris implements Serializable {
     public static final int NUTATIONS = 11;
     public static final int LIBRATIONS = 12;
     public static final int LAST_COMPONENT = 12;
+
+    /**
+     * Constructs a new JPLEphemeris object for a specified time span from a binary
+     * JPL ephemeris file.
+     *
+     * @param filename The name of the binary JPL ephemeris file from which the data
+     *                 will be loaded.
+     * @param jdstart The earliest time (expressed as a Julian Date in Barycentric Dynamical Time)
+     *                at which the ephemeris must be able to return positions and
+     *                velocities.
+     * @param jfinis The latest time (expressed as a Julian Date in Barycentric Dynamical Time)
+     *                at which the ephemeris must be able to return positions and
+     *                velocities.
+     */
 
     public JPLEphemeris(String filename, double jdstart, double jdfinis) throws
       IOException, JPLEphemerisException {
@@ -143,13 +179,61 @@ public class JPLEphemeris implements Serializable {
 		nCheby = offsets[i][1];
     }
 
+    /**
+     * Return the value of the AU parameter contained within the JPL ephemeris.
+     * This is the length of the astronomical unit expresssed in kilometres.
+     *
+     * @return The length of an astronomical unit,, in kilometres.
+     */
     public double getAU() { return AU; }
+
+    /**
+     * Return the value of the Earth/Moon mass ratio contained within the JPL
+     * ephemeris.
+     *
+     * @return The Earth/Moon mass ratio.
+     */
     public double getEMRAT() { return EMRAT; }
+
+    /**
+     * Return the DE ephemeris number of the JPL ephemeris which was used to
+     * create this object.
+     *
+     * @return The DE ephemeris number.
+     */
     public int getEphemerisNumber() { return numde; }
 
+    /**
+     * Return the earliest date for which this object can evaluate positions
+     * and velocities. Note that it may be earlier than the argument specified
+     * in the constructor, because the ephemeris data are stored as Chebyshev
+     * coefficients taken directly from the binary JPL ephemeris file.
+     *
+     * @return The earliest date for which this ephemeris is valid.
+     */
     public double getEarliestDate() { return limits[0]; }
+
+    /**
+     * Return the latest date for which this object can evaluate positions
+     * and velocities. Note that it may be later than the argument specified
+     * in the constructor, because the ephemeris data are stored as Chebyshev
+     * coefficients taken directly from the binary JPL ephemeris file.
+     *
+     * @return The latest date for which this ephemeris is valid.
+     */
     public double getLatestDate() { return limits[1]; }
 
+    /**
+     * Indicates whether the ephemeris can evaluate positions and velocities for
+     * the specified date.
+     *
+     * @param t The date which is to be tested.
+     *
+     * @return True of the ephemeris covers the specified date, false otherwise.
+     *         If this function returns false, then an exception will be thrown
+     *         if the user attempts to evaluate positions and velocities at the
+     *         specified date.
+     */
     public boolean isValidDate(double t) {
 	if (t < limits[0] || t > limits[1])
 	    return false;
@@ -157,6 +241,21 @@ public class JPLEphemeris implements Serializable {
 	    return true;
     }
 
+    /**
+     * Indicates whether the ephemeris contains Chebyshev coefficients for the
+     * component identified by the specified code.
+     *
+     * @param k The code which specifies the planet or quantity (such as librations)
+     *          to be tested. This will normally be one of the public constants
+     *          such as JPLEphemeris.LIBRATIONS. All ephemerides contain Chebyshev
+     *          coefficients for the Sun, Moon and planets; they do not all contain
+     *          coefficients for nutations and librations.
+     *
+     * @return True if the ephemeris contains Chebyshev coefficients for the specified
+     *         component; false otherwise. Note that if the user attempts to evaluate
+     *         the ephemeris for a component which is not present, an exception will
+     *         be thrown.
+     */
     public boolean hasComponent(int k) {
 	if (k < 0 || k > LIBRATIONS)
 	    return false;
@@ -167,10 +266,20 @@ public class JPLEphemeris implements Serializable {
 	    return false;
     }
 
+    /**
+     * Returns the number of data records which this object contains.
+     *
+     * @return The number of data records.
+     */
     public int getNumberOfDataRecords() {
 	return data.length;
     }
 
+    /**
+     * Returns the length of the data records in this object.
+     *
+     * @return The number of double-precision values in each data record.
+     */
     public int getLengthOfDataRecord() {
 	return data[0].length;
     }
@@ -196,7 +305,27 @@ public class JPLEphemeris implements Serializable {
 	}
     }
 
-
+    /**
+     * This is the main routine which most client programs will call in order to
+     * evaluate the ephemeris for a specified component at a specified date.
+     *
+     * @param jd The time argument, expressed as a Julian Date in Barycentric
+     *           Dynamical Time, at which the ephemeris is to be evaluated.
+     *
+     * @param body The code representing the component (Sun, Moon, planet, nutation,
+     *             libration) which is to be evaluated. This should be one of the
+     *             public constants such as JPLEphemeris.JUPITER.
+     *
+     * @param pos The output array into which the position vector components will
+     *            be stored. The length of this array must be at least three for
+     *            planetary positions, and at least two for nutations and librations.
+     *
+     * @param vel The output array into which the velocity vector components will
+     *            be stored. This argument may be null, indicating that the velocity
+     *            is not required. However, if it is non-null, then its length must
+     *            be at least three for planetary positions and at least two for
+     *            nutations and librations.
+     */
     public void calculatePositionAndVelocity(double jd, int nBody, double []pos, double []vel) 
       throws JPLEphemerisException {
 	if (!hasComponent(nBody))
