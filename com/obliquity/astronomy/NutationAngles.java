637274d2f9fecbd3ae6fcff394115d85b7ecd331
David Harper <david@obliquity.net>
2004-05-05 09:11:48 +0000

    Added classes to represent precession angles and nutation angles.
    git-svn-id: file:///disk2/repository/svn/trunk/astrojava@144 32807195-9110-437f-9ebb-0d7c4c47859f

---
+++ com/obliquity/astronomy/NutationAngles.java
@@ -0,0 +1,27 @@
+package com.obliquity.astronomy;
+
+public class NutationAngles {
+    private double dpsi, deps;
+
+    public NutationAngles() {
+	dpsi = deps = 0.0;
+    }
+
+    public NutationAngles(double dpsi, double deps) {
+	this.dpsi = dpsi;
+	this.deps = deps;
+    }
+
+    public void setAngles(double dpsi, double deps) {
+	this.dpsi = dpsi;
+	this.deps = deps;
+    }
+
+    public void setDpsi(double dpsi) { this.dpsi = dpsi; }
+
+    public double getDpsi() { return dpsi; }
+
+    public void setDeps(double deps) { this.deps = deps; }
+
+    public double getDeps() { return deps; }
+}
---
+++ com/obliquity/astronomy/PrecessionAngles.java
@@ -0,0 +1,33 @@
+package com.obliquity.astronomy;
+
+public class PrecessionAngles {
+    private double zeta, z, theta;
+
+    public PrecessionAngles() {
+	zeta = z = theta = 0.0;
+    }
+
+    public PrecessionAngles(double zeta, double z, double theta) {
+	this.zeta = zeta;
+	this.z = z;
+	this.theta = theta;
+    }
+
+    public void setAngles(double zeta, double z, double theta) {
+	this.zeta = zeta;
+	this.z = z;
+	this.theta = theta;
+    }
+
+    public void setZeta(double zeta) { this.zeta = zeta; }
+
+    public double getZeta() { return zeta; }
+
+    public void setZ(double z) { this.z = z; }
+
+    public double getZ() { return z; }
+
+    public void setTheta(double theta) { this.theta = theta; }
+
+    public double getTheta() { return theta; }
+}
