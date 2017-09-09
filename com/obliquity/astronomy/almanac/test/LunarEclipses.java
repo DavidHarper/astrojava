9c89991e2658da93b9e99afb48a0668e3c50b297
David Harper <david@obliquity.net>
2017-09-09 15:22:19 +0100

    Preliminary version of class for exploring lunar eclipses.

---
+++ com/obliquity/astronomy/almanac/test/LunarEclipses.java
@@ -0,0 +1,223 @@
+/*
+ * astrojava - a package for reading JPL ephemeris files
+ *
+ * Copyright (C) 2006-2014 David Harper at obliquity.com
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA  02111-1307, USA.
+ *
+ * See the COPYING file located in the top-level-directory of
+ * the archive of this library for complete text of license.
+ */
+
+package com.obliquity.astronomy.almanac.test;
+
+import static java.lang.Math.*;
+
+import java.io.IOException;
+import java.text.DecimalFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
+
+import com.obliquity.astronomy.almanac.*;
+
+public class LunarEclipses {
+	private static final char TAB = '\t';
+	
+	private static final double TWO_PI = 2.0 * PI;
+	
+	private static final double LUNAR_MONTH = 29.53059;
+	
+	private static final double EARTH_RADIUS = 6378.0, MOON_RADIUS = 1737.0, SUN_RADIUS = 695700.0;
+
+	private final DecimalFormat dfmta = new DecimalFormat("#0.000");
+	
+	private final double AU;
+	
+	private EarthRotationModel erm = new IAUEarthRotationModel();
+	
+	private ApparentPlace apSun, apMoon;
+	
+	public static void main(String args[]) {
+		SimpleDateFormat datefmt = new SimpleDateFormat("yyyy-MM-dd");
+		datefmt.setTimeZone(TimeZone.getTimeZone("GMT"));
+		
+		final double UNIX_EPOCH_AS_JD = 2440587.5;
+		final double MILLISECONDS_PER_DAY = 1000.0 * 86400.0;
+	
+		String filename = null;
+		String startdate = null;
+		String enddate = null;
+
+		for (int i = 0; i < args.length; i++) {
+			if (args[i].equalsIgnoreCase("-ephemeris"))
+				filename = args[++i];
+
+			if (args[i].equalsIgnoreCase("-startdate"))
+				startdate = args[++i];
+
+			if (args[i].equalsIgnoreCase("-enddate"))
+				enddate = args[++i];
+		}
+
+		if (filename == null || startdate == null || enddate == null) {
+			showUsage();
+			System.exit(1);
+		}
+		
+		Date date = null;
+		
+		try {
+			date = datefmt.parse(startdate);
+		} catch (ParseException e) {
+			System.err.println("Failed to parse \"" + startdate + "\" as an ISO date");
+			e.printStackTrace();
+			System.exit(1);
+		}
+		
+		double jdstart = UNIX_EPOCH_AS_JD + ((double)date.getTime())/MILLISECONDS_PER_DAY;
+		
+		try {
+			date = datefmt.parse(enddate);
+		} catch (ParseException e) {
+			System.err.println("Failed to parse \"" + enddate + "\" as an ISO date");
+			e.printStackTrace();
+			System.exit(1);
+		}
+		
+		double jdfinish = UNIX_EPOCH_AS_JD + ((double)date.getTime())/MILLISECONDS_PER_DAY + LUNAR_MONTH;
+		
+		JPLEphemeris ephemeris = null;
+
+		try {
+			ephemeris = new JPLEphemeris(filename, jdstart - 1.0,
+					jdfinish + 1.0);
+		} catch (JPLEphemerisException jee) {
+			jee.printStackTrace();
+			System.err.println("JPLEphemerisException ... " + jee);
+			System.exit(1);
+		} catch (IOException ioe) {
+			ioe.printStackTrace();
+			System.err.println("IOException ... " + ioe);
+			System.exit(1);
+		}
+
+		MoonPhases mp = new MoonPhases(ephemeris);
+		
+		LunarEclipses le = new LunarEclipses(ephemeris);
+		
+		double t = jdstart;
+		
+		datefmt = new SimpleDateFormat("yyyy-MM-dd HH:mm");
+		
+		while (t < jdfinish) {
+			try {
+				t = mp.getDateOfNextPhase(t, MoonPhases.FULL_MOON);
+			} catch (JPLEphemerisException e) {
+				e.printStackTrace();
+				System.exit(1);
+			}
+			
+			double dticks = MILLISECONDS_PER_DAY * (t - UNIX_EPOCH_AS_JD);
+			
+			long ticks = (long)dticks;
+			
+			date = new Date(ticks);
+			
+			System.out.println(datefmt.format(date));
+			
+			try {
+				le.testForLunarEclipse(t);
+			} catch (JPLEphemerisException e) {
+				e.printStackTrace();
+				System.exit(1);
+			}
+			
+			t += 29.0;
+		}
+	}
+
+	public static void showUsage() {
+		System.err.println("MANDATORY PARAMETERS");
+		System.err.println("\t-ephemeris\tName of ephemeris file");
+		System.err.println("\t-startdate\tStart date");
+		System.err.println("\t-enddate\tEnd date");
+	}
+
+	public LunarEclipses(JPLEphemeris ephemeris) {
+		MovingPoint moon = new MoonCentre(ephemeris);
+		
+		MovingPoint earth = new EarthCentre(ephemeris);
+		
+		MovingPoint sun = new PlanetCentre(ephemeris, JPLEphemeris.SUN);
+		
+		apSun = new ApparentPlace(earth, sun, sun, erm);
+
+		apMoon = new ApparentPlace(earth, moon, sun, erm);
+		
+		AU = ephemeris.getAU();
+	}
+
+	public void testForLunarEclipse(double t) throws JPLEphemerisException {
+		apSun.calculateApparentPlace(t);
+		apMoon.calculateApparentPlace(t);
+		
+		double rSun = AU * apSun.getGeometricDistance();
+		double rMoon = AU * apMoon.getGeometricDistance();
+		
+		System.out.println(TAB + "r(Sun)  = " + rSun);
+		System.out.println(TAB + "r(Moon) = " + rMoon);
+		
+		// Half-angle at vertex of umbral cone
+		double theta = asin((SUN_RADIUS - EARTH_RADIUS)/rSun);
+		
+		// Complement of theta
+		double beta = 0.5 * PI - theta;
+		
+		double alpha1 = acos((EARTH_RADIUS - MOON_RADIUS)/rMoon);
+		
+		double gamma1 = beta - alpha1;
+		gamma1 *= 180.0/PI;
+		
+		double alpha2 = acos((EARTH_RADIUS + MOON_RADIUS)/rMoon);
+		
+		double gamma2 = beta - alpha2;
+		gamma2 *= 180.0/PI;
+		
+		double raSun = apSun.getRightAscensionOfDate();
+		
+		double decSun = apSun.getDeclinationOfDate();
+		
+		System.out.println(TAB + " raSun  = " + dfmta.format(12.0 * raSun/PI));
+		System.out.println(TAB + " decSun = " + dfmta.format(180.0 * decSun/PI));
+		
+		double raMoon = apMoon.getRightAscensionOfDate();
+		
+		double decMoon = apMoon.getDeclinationOfDate();
+		
+		System.out.println(TAB + " raMoon  = " + dfmta.format(12.0 * raMoon/PI));
+		System.out.println(TAB + " decMoon = " + dfmta.format(180.0 * decMoon/PI));
+
+		double q = sin(decMoon) * sin(decSun) + cos(decMoon) * cos(decSun) * cos(raMoon-raSun);
+		
+		q = 180.0 - acos(q) * 180.0/PI;
+		
+		System.out.println(TAB + " gamma1 = " + dfmta.format(gamma1));
+		System.out.println(TAB + " gamma2 = " + dfmta.format(gamma2));
+		System.out.println(TAB + " elong  = " + dfmta.format(q));
+	} 
+}
