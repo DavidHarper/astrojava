eb57740506cc5b5700bb362bde32e5bebb8e455e
David Harper <david@obliquity.net>
2018-03-03 20:14:20 +0000

    Make shell scripts location-independent. Add sanity checking.

--- almanac.sh
+++ almanac.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
-java ${JAVA_OPTS} -jar almanac.jar "$@"
+SCRIPT_DIR=`dirname $0`
+
+JAR_FILE=${SCRIPT_DIR}/almanac.jar
+
+if [ ! -f ${JAR_FILE} ]
+then
+  echo "Cannot find JAR file ${JAR_FILE}. Run the build script."
+  exit 1
+fi
+
+java ${JAVA_OPTS} -jar ${JAR_FILE} "$@"
--- conjunction.sh
+++ conjunction.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+SCRIPT_DIR=`dirname $0`
+
+JAR_FILE=${SCRIPT_DIR}/almanac.jar
+
+if [ -f ${JAR_FILE} ]
+then
+  CLASSPATH=${JAR_FILE}
+else
+  CLASSPATH=${SCRIPT_DIR}
+fi
+
 MAIN_CLASS=com.obliquity.astronomy.almanac.test.ConjunctionFinder
 
-java ${JAVA_OPTS} ${MAIN_CLASS} "$@"
+java ${JAVA_OPTS} -classpath ${CLASSPATH} ${MAIN_CLASS} "$@"
