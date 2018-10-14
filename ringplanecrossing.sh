4e99387e76d10bd0d3e6eb5b19e500f3e0f04700
L.M.Stockman <l.m.stockman@obliquity.net>
2018-10-14 14:48:39 +0100

    Add utility scripts.

---
+++ ringplanecrossing.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+SCRIPT_DIR=`dirname $0`
+
+JAR_FILE=${SCRIPT_DIR}/almanac.jar
+
+RPXCLASS=com.obliquity.astronomy.almanac.test.SaturnRingPlaneCrossingFinder
+
+if [ ! -f ${JAR_FILE} ]
+then
+  echo "Cannot find JAR file ${JAR_FILE}. Run the build script."
+  exit 1
+fi
+
+if [ ! -z "${EPHEMERIS_FILE}" -a -f "${EPHEMERIS_FILE}" ]
+then
+  EPHEMERIS_OPTS="-ephemeris ${EPHEMERIS_FILE}"
+fi
+
+java ${JAVA_OPTS} -classpath ${JAR_FILE} ${RPXCLASS} ${EPHEMERIS_OPTS} "$@"
