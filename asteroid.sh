3d8be0dccdee2bff321221f50b66fc3aaa9dc9e2
David Harper <david@obliquity.net>
2019-11-24 08:55:57 +0000

    Add wrapper script for asteroid ephemeris reader.

---
+++ asteroid.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+SCRIPT_DIR=`dirname $0`
+
+JAR_FILE=${SCRIPT_DIR}/almanac.jar
+
+if [ -f ${JAR_FILE} ]
+then
+  CLASSPATH=${JAR_FILE}
+else
+  CLASSPATH=${SCRIPT_DIR}
+fi
+
+MAIN_CLASS=com.obliquity.astronomy.almanac.test.AsteroidEphemerisReader
+
+java ${JAVA_OPTS} -classpath ${CLASSPATH} ${MAIN_CLASS} "$@"
