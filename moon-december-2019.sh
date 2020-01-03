b317f38269a2d034573d93a14e95f3b283cea9b6
David Harper <david@obliquity.net>
2020-01-03 08:20:37 +0000

    Remove hard-coded path to ephemeris file.

--- moon-december-2019.sh
+++ moon-december-2019.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
+if [ ! -z "${EPHEMERIS_FILE}" -a -f "${EPHEMERIS_FILE}" ]
+then
+  EPHEMERIS_OPTS="-ephemeris ${EPHEMERIS_FILE}"
+fi
+
 java com.obliquity.astronomy.almanac.test.GreatCircleFlight \
-  -ephemeris /Users/adh/data/jpl/de406/unxp1800.406 \
+  ${EPHEMERIS_OPTS} \
   -startdate '2019-12-12 14:00' -startpos -5.71,57.8 \
   -enddate '2019-12-12 20:00' -endpos -107.25,60.12 \
-  -target moon
+  -target moon \
+  "$@"
--- venus-december-2013.sh
+++ venus-december-2013.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
-java -Ddebug=true com.obliquity.astronomy.almanac.test.GreatCircleFlight -ephemeris ~/data/jpl/de406/unxp1800.406 -startdate '2013-12-18 15:30' -startpos 0,51.5 -enddate '2013-12-19 01:00' -endpos -120,48 -target venus
+if [ ! -z "${EPHEMERIS_FILE}" -a -f "${EPHEMERIS_FILE}" ]
+then
+  EPHEMERIS_OPTS="-ephemeris ${EPHEMERIS_FILE}"
+fi
 
-java -Ddebug=true com.obliquity.astronomy.almanac.test.GreatCircleFlight -ephemeris ~/data/jpl/de406/unxp1800.406 -startdate '2013-12-18 15:30' -startpos 0,51.5 -enddate '2013-12-19 01:00' -endpos -120,48 -target sun
+java -Ddebug=true com.obliquity.astronomy.almanac.test.GreatCircleFlight ${EPHEMERIS_OPTS} \
+    -startdate '2013-12-18 15:30' -startpos 0,51.5 -enddate '2013-12-19 01:00' -endpos -120,48 \
+    -target venus \
+    "$@"
+
+java -Ddebug=true com.obliquity.astronomy.almanac.test.GreatCircleFlight ${EPHEMERIS_OPTS} \
+    -startdate '2013-12-18 15:30' -startpos 0,51.5 -enddate '2013-12-19 01:00' -endpos -120,48 \
+    -target sun \
+    "$@"
