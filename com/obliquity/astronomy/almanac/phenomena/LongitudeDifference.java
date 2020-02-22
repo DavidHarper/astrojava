4d9e29c249ad682afe40c8e55b13a50dfa38ab68
David Harper <david@obliquity.net>
2020-02-22 10:20:22 +0000

    Preliminary version of planetary phenomena finder classes.

---
+++ com/obliquity/astronomy/almanac/phenomena/LongitudeDifference.java
@@ -0,0 +1,100 @@
+/*
+ * astrojava - a package for reading JPL ephemeris files
+ *
+ * Copyright (C) 2006-2018 David Harper at obliquity.com
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA  02111-1307, USA.
+ *
+ * See the COPYING file located in the top-level-directory of
+ * the archive of this library for complete text of license.
+ */
+
+package com.obliquity.astronomy.almanac.phenomena;
+
+import com.obliquity.astronomy.almanac.ApparentPlace;
+import com.obliquity.astronomy.almanac.EarthRotationModel;
+import com.obliquity.astronomy.almanac.IAUEarthRotationModel;
+import com.obliquity.astronomy.almanac.JPLEphemerisException;
+
+public class LongitudeDifference implements TargetFunction {
+	private static final double TWO_PI = 2.0 * Math.PI;
+	
+	private ApparentPlace apTarget1 = null, apTarget2 = null;
+	
+	private EarthRotationModel erm = new IAUEarthRotationModel();
+	
+	private double targetDifference = 0.0;
+	
+	public LongitudeDifference(ApparentPlace apTarget1, ApparentPlace apTarget2, double targetDifference) throws PhenomenaException {
+		this.apTarget1 = apTarget1;
+		this.apTarget2 = apTarget2;
+		this.targetDifference = targetDifference;
+		
+		if (apTarget1.getObserver().getBodyCode() != apTarget2.getObserver().getBodyCode())
+			throw new PhenomenaException("Observer is not the same for both targets");
+	}
+	
+	public LongitudeDifference(ApparentPlace apTarget1, ApparentPlace apTarget2) throws PhenomenaException {
+		this(apTarget1, apTarget2, 0.0);
+	}
+	
+	private double calculateEclipticLongitude(double ra, double dec, double t) {
+		double xa = Math.cos(ra) * Math.cos(dec);
+		double ya = Math.sin(ra) * Math.cos(dec);
+		double za = Math.sin(dec);
+		
+		double obliquity = erm.meanObliquity(t);
+		
+		double xe = xa;
+		double ye = ya * Math.cos(obliquity) + za * Math.sin(obliquity);
+		
+		return Math.atan2(ye, xe);
+	}
+	
+	private double reduceAngle(double x) {
+		while (x > Math.PI)
+			x -= TWO_PI;
+		
+		while (x <= -Math.PI)
+			x+= TWO_PI;
+		
+		return x;
+	}
+
+	private double calculateDifferenceInLongitude(double t) throws JPLEphemerisException {
+		apTarget1.calculateApparentPlace(t);
+		
+		apTarget2.calculateApparentPlace(t);
+		
+		double ra1 = apTarget1.getRightAscensionOfDate();
+		
+		double dec1 = apTarget1.getDeclinationOfDate();
+		
+		double lambda1 = calculateEclipticLongitude(ra1, dec1, t);
+		
+		double ra2 = apTarget2.getRightAscensionOfDate();
+		
+		double dec2 = apTarget2.getDeclinationOfDate();
+		
+		double lambda2 = calculateEclipticLongitude(ra2, dec2, t);
+		
+		return reduceAngle(lambda2 - lambda1 - targetDifference);	
+	}
+
+	public double valueAtTime(double t) throws JPLEphemerisException {
+		return calculateDifferenceInLongitude(t);
+	}
+}
---
+++ com/obliquity/astronomy/almanac/phenomena/PhenomenaFinder.java
@@ -0,0 +1,311 @@
+/*
+ * astrojava - a package for reading JPL ephemeris files
+ *
+ * Copyright (C) 2006-2018 David Harper at obliquity.com
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA  02111-1307, USA.
+ *
+ * See the COPYING file located in the top-level-directory of
+ * the archive of this library for complete text of license.
+ */
+
+package com.obliquity.astronomy.almanac.phenomena;
+
+import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
+
+import com.obliquity.astronomy.almanac.ApparentPlace;
+import com.obliquity.astronomy.almanac.AstronomicalDate;
+import com.obliquity.astronomy.almanac.EarthCentre;
+import com.obliquity.astronomy.almanac.EarthRotationModel;
+import com.obliquity.astronomy.almanac.IAUEarthRotationModel;
+import com.obliquity.astronomy.almanac.JPLEphemeris;
+import com.obliquity.astronomy.almanac.JPLEphemerisException;
+import com.obliquity.astronomy.almanac.MoonCentre;
+import com.obliquity.astronomy.almanac.MovingPoint;
+import com.obliquity.astronomy.almanac.PlanetCentre;
+
+public class PhenomenaFinder {
+	private static final double UNIX_EPOCH_AS_JD = 2440587.5;
+	private static final double MILLISECONDS_PER_DAY = 1000.0 * 86400.0;
+	
+	private static final SimpleDateFormat datefmtIn = new SimpleDateFormat(
+			"yyyy-MM-dd");
+	
+	public static void main(String args[]) {
+		datefmtIn.setTimeZone(TimeZone.getTimeZone("GMT"));
+
+		String filename = null;
+		String body1name = null;
+		String body2name = null;
+		String startdate = null;
+		String enddate = null;
+		String stepsize = null;
+
+		for (int i = 0; i < args.length; i++) {
+			if (args[i].equalsIgnoreCase("-ephemeris"))
+				filename = args[++i];
+
+			if (args[i].equalsIgnoreCase("-body1"))
+				body1name = args[++i];
+
+			if (args[i].equalsIgnoreCase("-body2"))
+				body2name = args[++i];
+
+			if (args[i].equalsIgnoreCase("-startdate"))
+				startdate = args[++i];
+
+			if (args[i].equalsIgnoreCase("-enddate"))
+				enddate = args[++i];
+
+			if (args[i].equalsIgnoreCase("-step"))
+				stepsize = args[++i];
+		}
+
+		if (filename == null || body1name == null || body2name == null || startdate == null
+				|| enddate == null) {
+			showUsage();
+			System.exit(1);
+		}
+
+		int kBody1 = parseBody(body1name);
+
+		if (kBody1 < 0) {
+			System.err.println("Unknown name for -body1: \"" + body1name + "\"");
+			System.exit(1);
+		}
+
+		int kBody2 = parseBody(body2name);
+
+		if (kBody2 < 0) {
+			System.err.println("Unknown name for -body2: \"" + body2name + "\"");
+			System.exit(1);
+		}
+		
+		if (kBody1 == kBody2) {
+			System.err.println("Target bodies are the same.");
+			System.exit(1);
+		}
+
+		Date date = null;
+
+		try {
+			date = datefmtIn.parse(startdate);
+		} catch (ParseException e) {
+			System.err.println(
+					"Failed to parse \"" + startdate + "\" as an ISO date");
+			e.printStackTrace();
+			System.exit(1);
+		}
+
+		double jdstart = UNIX_EPOCH_AS_JD
+				+ ((double) date.getTime()) / MILLISECONDS_PER_DAY;
+
+		try {
+			date = datefmtIn.parse(enddate);
+		} catch (ParseException e) {
+			System.err.println(
+					"Failed to parse \"" + enddate + "\" as an ISO date");
+			e.printStackTrace();
+			System.exit(1);
+		}
+
+		double jdfinish = UNIX_EPOCH_AS_JD
+				+ ((double) date.getTime()) / MILLISECONDS_PER_DAY;
+
+		double jdstep = (stepsize == null) ? 1.0 : Double.parseDouble(stepsize);
+
+		JPLEphemeris ephemeris = null;
+
+		try {
+			ephemeris = new JPLEphemeris(filename, jdstart - 1.0,
+					jdfinish + 1.0);
+		} catch (JPLEphemerisException jee) {
+			jee.printStackTrace();
+			System.err.println("JPLEphemerisException ... " + jee);
+			System.exit(1);
+		} catch (IOException ioe) {
+			ioe.printStackTrace();
+			System.err.println("IOException ... " + ioe);
+			System.exit(1);
+		}
+
+		MovingPoint planet1 = null;
+
+		if (kBody1 == JPLEphemeris.MOON)
+			planet1 = new MoonCentre(ephemeris);
+		else
+			planet1 = new PlanetCentre(ephemeris, kBody1);
+
+		MovingPoint planet2 = null;
+
+		if (kBody2 == JPLEphemeris.MOON)
+			planet2 = new MoonCentre(ephemeris);
+		else
+			planet2 = new PlanetCentre(ephemeris, kBody2);
+
+		EarthCentre earth = new EarthCentre(ephemeris);
+
+		MovingPoint sun = null;
+
+		if (kBody1 == JPLEphemeris.SUN)
+			sun = planet1;
+		else if (kBody2 == JPLEphemeris.SUN)
+			sun = planet2;
+		else
+			sun = new PlanetCentre(ephemeris, JPLEphemeris.SUN);
+
+		EarthRotationModel erm = new IAUEarthRotationModel();
+
+		ApparentPlace apTarget1 = new ApparentPlace(earth, planet1, sun, erm);
+
+		ApparentPlace apTarget2 = new ApparentPlace(earth, planet2, sun, erm);
+
+		LongitudeDifference ldiff = null;
+		
+		try {
+			ldiff = new LongitudeDifference(apTarget1, apTarget2);
+		} catch (PhenomenaException e1) {
+			e1.printStackTrace();
+			System.exit(1);
+		}
+		
+		PhenomenaFinder finder = new PhenomenaFinder();
+		
+		try {
+			finder.run(ldiff, jdstart, jdfinish, jdstep);
+		} catch (JPLEphemerisException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private boolean changeOfSign(double x1, double x2) {
+		if (x1 > 0.0 && x2 > 0.0)
+			return false;
+		
+		if (x1 < 0.0 && x2 < 0.0)
+			return false;
+	
+		// Exclude signs change between -PI and +PI
+		if (Math.abs(x1) > 2.0 || Math.abs(x2) > 2.0)
+			return false;
+		
+		return true;
+	}
+
+	private void run(LongitudeDifference ldiff, double jdstart, double jdfinish,
+			double jdstep) throws JPLEphemerisException {
+		double lastDX = Double.NaN;
+		boolean first = true;
+		
+		for (double t = jdstart; t <= jdfinish; t += jdstep) {
+			double dX = ldiff.valueAtTime(t);
+			
+			if (!first) {
+				if (changeOfSign(lastDX, dX)) {
+					double tLast = t - jdstep;
+					
+					double tExact = findExactInstant(ldiff, tLast, t);
+					
+					AstronomicalDate ad = new AstronomicalDate(tExact);
+					
+					System.out.printf("%5d %02d %02d %02d:%02d\n", ad.getYear(), ad.getMonth(), ad.getDay(), ad.getHour(), ad.getMinute());
+				}
+			}
+			
+			lastDX = dX;
+			first = false;
+		}		
+	}
+	
+	// Limit of difference in RA for convergence
+	private final double EPSILON = 0.1 * (Math.PI/180.0)/3600.0;
+	
+	private double findExactInstant(LongitudeDifference ldiff, double t1, double t2) throws JPLEphemerisException {
+		while (true) {
+			double dX1 = ldiff.valueAtTime(t1);
+	
+			double dX2 = ldiff.valueAtTime(t2);
+		
+			double dXchange = dX2 - dX1;
+		
+			double dXrate = dXchange/(t2 - t1);
+		
+			double tNew = t1 - dX1/dXrate;
+		
+			double dX3 = ldiff.valueAtTime(tNew);
+
+			if (Math.abs(dX3) < EPSILON)
+				return tNew;
+			
+			if (changeOfSign(dX1, dX3))
+				t2 = tNew;
+			else
+				t1 = tNew;
+		}
+	}
+
+	public static void showUsage() {
+		String[] lines = { "MANDATORY PARAMETERS",
+				"\t-ephemeris\tName of ephemeris file",
+				"\t-body1\t\tName of body 1",
+				"\t-body2\t\tName of body 2",
+				"\t-startdate\tStart date",
+				"\t-enddate\tEnd date",
+		};
+		
+		for (String line : lines)
+			System.err.println(line);
+	}
+
+	private static int parseBody(String bodyname) {
+		if (bodyname.equalsIgnoreCase("sun"))
+			return JPLEphemeris.SUN;
+
+		if (bodyname.equalsIgnoreCase("moon"))
+			return JPLEphemeris.MOON;
+
+		if (bodyname.equalsIgnoreCase("mercury"))
+			return JPLEphemeris.MERCURY;
+
+		if (bodyname.equalsIgnoreCase("venus"))
+			return JPLEphemeris.VENUS;
+
+		if (bodyname.equalsIgnoreCase("mars"))
+			return JPLEphemeris.MARS;
+
+		if (bodyname.equalsIgnoreCase("jupiter"))
+			return JPLEphemeris.JUPITER;
+
+		if (bodyname.equalsIgnoreCase("saturn"))
+			return JPLEphemeris.SATURN;
+
+		if (bodyname.equalsIgnoreCase("uranus"))
+			return JPLEphemeris.URANUS;
+
+		if (bodyname.equalsIgnoreCase("neptune"))
+			return JPLEphemeris.NEPTUNE;
+
+		if (bodyname.equalsIgnoreCase("pluto"))
+			return JPLEphemeris.PLUTO;
+
+		return -1;
+	}
+
+}
