aedb64148cbd88918a7b36bbd39c3121bd28ce3b
David Harper <david@obliquity.net>
2020-02-23 16:36:38 +0000

    Add target function representing RA of a planet.

---
+++ com/obliquity/astronomy/almanac/phenomena/RightAscensionFunction.java
@@ -0,0 +1,43 @@
+/*
+ * astrojava - a package for reading JPL ephemeris files
+ *
+ * Copyright (C) 2006-2018 David Harper at obliquity.com
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA  02111-1307, USA.
+ *
+ * See the COPYING file located in the top-level-directory of
+ * the archive of this library for complete text of license.
+ */
+
+package com.obliquity.astronomy.almanac.phenomena;
+
+import com.obliquity.astronomy.almanac.ApparentPlace;
+import com.obliquity.astronomy.almanac.JPLEphemerisException;
+
+public class RightAscensionFunction implements TargetFunction {
+	private ApparentPlace apTarget = null;
+	
+	public RightAscensionFunction(ApparentPlace apTarget) {
+		this.apTarget = apTarget;
+	}
+
+	public double valueAtTime(double t) throws JPLEphemerisException {
+		apTarget.calculateApparentPlace(t);
+		
+		return apTarget.getRightAscensionOfDate();
+	}
+
+}
