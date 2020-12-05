9098bd1fd0c725693f54df2fb05d9e9742c2a75c
David Harper <david@obliquity.net>
2020-12-05 14:37:11 +0000

    Add class to calculate stellar apparent places rigorously.

---
+++ src/main/java/com/obliquity/astronomy/almanac/StarApparentPlace.java
@@ -0,0 +1,185 @@
+/*
+ * astrojava - a package for reading JPL ephemeris files
+ *
+ * Copyright (C) 2006-2020 David Harper at obliquity.com
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA  02111-1307, USA.
+ *
+ * See the COPYING file located in the top-level-directory of
+ * the archive of this library for complete text of license.
+ */
+
+package com.obliquity.astronomy.almanac;
+
+public class StarApparentPlace {
+	protected MovingPoint observer;
+	protected MovingPoint sun;
+	protected EarthRotationModel erm;
+	
+	private boolean verbose = Boolean.getBoolean("starapparentplace.verbose");
+
+	public StarApparentPlace(MovingPoint observer, 	MovingPoint sun, EarthRotationModel erm) {
+		this.observer = observer;
+		this.sun = sun;
+		this.erm = erm;
+	}
+	
+	public Vector calculateApparentPlace(double ra, double dec, double parallax, double pmRA, double pmDec, double rv,
+			double fixedEpoch, double jd) throws JPLEphemerisException {
+		Vector q = new Vector(Math.cos(dec) * Math.cos(ra), Math.cos(dec) * Math.sin(ra), Math.sin(dec));
+		
+		if (verbose)
+			displayVector("CATALOGUE POSITION", q);
+		
+		double T = (jd - fixedEpoch)/36525.0;
+		
+		// Convert proper motions and parallax from mas/yr and mas to radians/cy and radians.
+		pmRA *= Math.PI/(180.0 * 3600.0 * 1000.0) * 100.0;
+		pmDec *= Math.PI/(180.0 * 3600.0 * 1000.0) * 100.0;
+		parallax *= Math.PI/(180.0 * 3600.0 * 1000.0);
+		
+		// Convert radial velocity from km/s to AU/cy.
+		rv *= 21.095;
+		
+		if (verbose)
+			System.out.printf("pmRA = %11.9f\npmDec = %11.9f\nparallax = %11.9f\n", pmRA, pmDec, parallax);
+				
+		double mx = -pmRA * Math.cos(dec) * Math.sin(ra) - pmDec * Math.sin(dec) * Math.cos(ra) + rv * parallax * q.getX();
+		double my =  pmRA * Math.cos(dec) * Math.cos(ra) - pmDec * Math.sin(dec) * Math.sin(ra) + rv * parallax * q.getY();
+		double mz =  pmDec * Math.cos(dec) + rv * parallax * q.getZ();
+		
+		Vector m = new Vector(mx, my, mz);
+		
+		if (verbose)
+			displayVector("SPACE MOTION", m);
+		
+		StateVector sve = observer.getStateVector(jd);
+		
+		Vector pEarth = sve.getPosition();
+		Vector vEarth = sve.getVelocity();
+		
+		Vector pSun = sun.getPosition(jd);
+		
+		if (verbose) {
+			displayVector("EARTH BARYCENTRIC POSITION", pEarth);
+			displayVector("EARTH BARYCENTRIC VELOCITY", vEarth);
+			displayVector("SUN   BARYCENTRIC POSITION", pSun);
+		}
+		
+		Vector P = pEarth.copyOf();
+		
+		P.multiplyBy(-parallax);
+
+		m.multiplyBy(T);
+		
+		P.add(m);
+		
+		P.add(q);
+		
+		if (verbose)
+			displayVector("P", P);
+		
+		Vector E = pEarth.copyOf();
+		
+		E.subtract(pSun);
+		
+		if (verbose)
+			displayVector("E = E_b - S_b", E);
+		
+		double emag = E.magnitude();
+		
+		E.normalise();
+		P.normalise();
+		
+		if (verbose) {
+			System.out.printf("|E| = %11.9f\n", emag);
+			displayVector("Normalised E", E);
+			displayVector("Normalised P", P);
+		}
+		
+		double pdote = P.scalarProduct(E);
+		
+		Vector dp = Vector.linearCombination(E, 1.0, P, -pdote);
+		
+		double factor = 2.0 * 9.87e-9 * emag/(1.0 + pdote);
+		
+		dp.multiplyBy(factor);
+		
+		Vector p1 = P.copyOf();
+		
+		p1.add(dp);
+		
+		if (verbose) {
+			System.out.printf("p . e = %11.9f\n", pdote);
+			displayVector("dP", dp);
+			displayVector("p1", p1);
+		}
+		
+		Vector V = vEarth.copyOf();
+		
+		V.multiplyBy(0.005775);
+		
+		if (verbose)
+			displayVector("V", V);
+		
+		double vmag = V.magnitude();
+		
+		double beta = 1.0/Math.sqrt(1.0 - vmag * vmag);
+		
+		double p1dotv = p1.scalarProduct(V);
+		
+		factor = 1.0 + p1dotv/(1.0 + 1.0/beta);
+		
+		if (verbose)
+			System.out.printf("|V| = %11.9f, beta = %11.9f, p1 . V = %11.9f, factor = %11.9f\n", vmag, beta, p1dotv, factor);
+		
+		Vector p2 = Vector.linearCombination(p1, 1.0/beta, V, factor);
+		
+		p2.multiplyBy(1.0/(1.0 + p1dotv));
+		
+		if (verbose)
+			displayVector("p2", p2);
+		
+		Matrix precession = erm.precessionMatrix(fixedEpoch, jd);
+		
+		Matrix nutation = erm.nutationMatrix(jd);
+
+		precession.rightMultiplyBy(nutation);
+		
+		if (verbose)
+			displayMatrix("PRECESSION-NUTATION", precession);
+		
+		p2.multiplyBy(precession);
+		
+		if (verbose)
+			displayVector("p3", p2);
+		
+		p2.normalise();
+		
+		return p2;
+	}
+	
+	private void displayVector(String caption, Vector v) {
+		System.out.println(caption);
+		System.out.printf("\t[ %12.9f  %12.9f  %12.9f ]\n", v.getX(), v.getY(), v.getZ());
+	}
+	
+	private void displayMatrix(String caption, Matrix m) {
+		System.out.println(caption);
+		for (int i = 0; i < 3; i++)
+			System.out.printf("\t [ %12.9f  %12.9f  %12.9f ]\n", m.getComponent(i, 0), m.getComponent(i, 1), m.getComponent(i, 2));
+	}
+}
---
+++ src/main/java/com/obliquity/astronomy/almanac/test/TestStarApparentPlace.java
@@ -0,0 +1,151 @@
+/*
+ * astrojava - a package for reading JPL ephemeris files
+ *
+ * Copyright (C) 2006-2020 David Harper at obliquity.com
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA  02111-1307, USA.
+ *
+ * See the COPYING file located in the top-level-directory of
+ * the archive of this library for complete text of license.
+ */
+
+package com.obliquity.astronomy.almanac.test;
+
+import java.io.IOException;
+
+import com.obliquity.astronomy.almanac.EarthCentre;
+import com.obliquity.astronomy.almanac.IAUEarthRotationModel;
+import com.obliquity.astronomy.almanac.JPLEphemeris;
+import com.obliquity.astronomy.almanac.JPLEphemerisException;
+import com.obliquity.astronomy.almanac.MovingPoint;
+import com.obliquity.astronomy.almanac.PlanetCentre;
+import com.obliquity.astronomy.almanac.StarApparentPlace;
+import com.obliquity.astronomy.almanac.Vector;
+
+public class TestStarApparentPlace {
+	private StarApparentPlace sap = null;
+	
+	public TestStarApparentPlace(StarApparentPlace sap) {
+		this.sap = sap;
+	}
+
+	public static void main(String[] args) {
+		String filename = null;
+		
+		for (int i = 0; i < args.length; i++) {
+			if (args[i].equalsIgnoreCase("-ephemeris"))
+				filename = args[++i];
+		}
+		
+		if (filename == null) {
+			System.err.println("You MUST specify an ephemeris filewith the -ephemeris option.");
+			System.exit(1);
+		}
+		
+		try {
+			JPLEphemeris ephemeris = new JPLEphemeris(filename);
+			
+			MovingPoint earth = new EarthCentre(ephemeris);
+			
+			MovingPoint sun = new PlanetCentre(ephemeris, JPLEphemeris.SUN);
+			
+			StarApparentPlace sap = new StarApparentPlace(earth, sun, new IAUEarthRotationModel());
+			
+			TestStarApparentPlace runner = new TestStarApparentPlace(sap);
+			
+			runner.run();
+
+		} catch (IOException | JPLEphemerisException e) {
+			e.printStackTrace();
+			System.exit(1);
+		}
+	}
+	
+	public void run() throws JPLEphemerisException {
+		// The Astronomical Almanac 1999, page B40
+		double ra = 14.0 + 39.0/60.0 + 36.087/3600.0;
+		ra *= Math.PI/12.0;
+		
+		double dec = 60.0 + 50.0/60.0 + 7.14/3600.0;
+		dec = -dec * Math.PI/180.0;
+		
+		double parallax = 752.0;
+		
+		double pmRA = -494.86 * 15.0;
+		
+		double pmDec = 696.0;
+		
+		double rv = -22.2;
+		
+		double fixedEpoch = 2451545.0;
+		
+		double jd = 2451179.5;
+		
+		Vector p = sap.calculateApparentPlace(ra, dec, parallax, pmRA, pmDec, rv, fixedEpoch, jd);
+		
+		displayEquatorialCoordinates(p);
+	}
+	
+	private void displayEquatorialCoordinates(Vector p) {
+		p.normalise();
+		
+		double ra = Math.atan2(p.getY(), p.getX());
+		
+		double dec = Math.asin(p.getZ());
+		
+		ra *= 12.0/Math.PI;
+		
+		if (ra < 0.0)
+			ra += 24.0;
+		
+		int rah = (int)ra;
+		
+		ra -= (double)rah;
+		
+		ra *= 60.0;
+		
+		int ram = (int)ra;
+		
+		ra -= (double)ram;
+		
+		double ras = 60.0 * ra;
+		
+		dec *= 180.0/Math.PI;
+		
+		char decp = dec > 0.0 ? 'N' : 'S';
+		
+		if (dec < 0.0)
+			dec = -dec;
+		
+		int decd = (int)dec;
+		
+		dec -= (double) decd;
+		
+		dec *= 60.0;
+		
+		int decm = (int)dec;
+		
+		dec -= (double)decm;
+		
+		double decs = 60.0 * dec;
+		
+		System.out.printf("    RA  %2d %02d %6.3f\n", rah, ram, ras);
+		System.out.printf("    Dec %2d %02d %5.2f %s\n", decd, decm, decs, decp);
+		
+		System.out.println();
+	}
+
+}
